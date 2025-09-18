092013b72db8aa58f1b8b063dfa41bb20d2f0888
auroratravelvn <22010338@st.phenikaa-uni.edu.vn>
2025-09-18 15:06:41 +0700

    Create Transmogrify.java

---
+++ ourProject/src/Transmogrify.java
@@ -0,0 +1,30 @@
+abstract class Actor{
+    abstract void act();
+}
+
+class HappyActor extends Actor{
+    public void act(){};
+}
+
+class SadActor extends Actor{
+    public void act(){};
+}
+
+class Stage{
+    Actor a = new HappyActor();
+    void change(){
+        a = new SadActor();
+    }
+    void go(){
+        a.act();
+    }
+}
+
+public class Transmogrify {
+    public static void main(String[] args) {
+        Stage s = new Stage();
+        s.go();
+        s.change();
+        s.go();
+    }
+}
