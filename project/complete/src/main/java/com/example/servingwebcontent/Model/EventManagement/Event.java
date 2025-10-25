60bd023979fbc9112533d242dd9fe74731c9895c
auroratravelvn <22010338@st.phenikaa-uni.edu.vn>
2025-10-25 22:04:06 +0700

    Create Event.java

---
+++ project/complete/src/main/java/com/example/servingwebcontent/Model/EventManagement/Event.java
@@ -0,0 +1,10 @@
+package com.example.servingwebcontent.Model.EventManagement;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "event")
+public class Event{
+
+}
